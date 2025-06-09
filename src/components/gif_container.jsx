f65003bec37612382f87e476c5a427359d239021
VitasGolden <114510780+VitasGolden@users.noreply.github.com>
2025-06-09 17:01:53 +0300

    Add dynamic GIF backgrounds based on accent color

--- src/components/gif_container.jsx
+++ src/components/gif_container.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../App.css'
 
 const GifContainer = () => {
-	return (
-		<div className='gif-container'>
-			<img src='src\assets\img\Green2.gif' alt='Опис GIF' />
-		</div>
-	)
+  const [currentGif, setCurrentGif] = useState('Green2.gif')
+
+  useEffect(() => {
+    // Load saved background GIF from localStorage
+    const savedGif = localStorage.getItem('backgroundGif')
+    if (savedGif) {
+      setCurrentGif(savedGif)
+    }
+
+    // Listen for theme changes
+    const handleThemeChange = (event) => {
+      const { backgroundGif } = event.detail
+      setCurrentGif(backgroundGif)
+    }
+
+    window.addEventListener('themeChange', handleThemeChange)
+
+    return () => {
+      window.removeEventListener('themeChange', handleThemeChange)
+    }
+  }, [])
+
+  return (
+    <div className='gif-container'>
+      <img src={`src/assets/img/${currentGif}`} alt='Background Animation' />
+    </div>
+  )
 }
 
 export default GifContainer
